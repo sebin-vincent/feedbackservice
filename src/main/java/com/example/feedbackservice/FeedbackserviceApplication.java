79e0554e422ffd02a0d62b52f32a16bea9a93642
sebin vincent <hellosebinvincent@gmail.com>
2020-04-10 12:40:16 +0530

    basic project skeleton added

---
+++ src/main/java/com/example/feedbackservice/FeedbackserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.feedbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackserviceApplication.class, args);
+	}
+
+}
